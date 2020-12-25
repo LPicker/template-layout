c41d8139f6c87d27b7b62f33ae5865ca4d56c853
LPicker <lianjianjie@foxmail.com>
2020-12-26 00:10:46 +0800

    header、footer切图

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const fs = require("fs");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TransferWebpackPlugin = require('transfer-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TransferWebpackPlugin = require("transfer-webpack-plugin");
 const htmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlBeautifyPlugin = require('@nurminen/html-beautify-webpack-plugin');
+const HtmlBeautifyPlugin = require("@nurminen/html-beautify-webpack-plugin");
 
 const getFileStr = (path) => {
   let str = "";
@@ -16,23 +16,31 @@ const getFileStr = (path) => {
 };
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   // entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: path.join(__dirname, "dist"),
+    watchContentBase: true,
+    compress: true,
+    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, 'dist'),
-      dry: false // 启用删除文件
+      root: path.resolve(__dirname, "dist"),
+      dry: false, // 启用删除文件
     }),
-    new TransferWebpackPlugin([
-      { from: 'src/css', to: '/css' }
-    ], path.resolve(__dirname, "./")),
+    new TransferWebpackPlugin(
+      [
+        { from: "src/css", to: "css" },
+        { from: "src/images", to: "images" },
+      ],
+      path.resolve(__dirname, "./")
+    ),
     new htmlWebpackPlugin({
       template: "src/pages/index.html",
       minify: false,
@@ -47,10 +55,10 @@ module.exports = {
           indent_with_tabs: false,
           indent_inner_html: true,
           preserve_newlines: true,
-          unformatted: ['p', 'i', 'b', 'span']
-        }
+          unformatted: ["p", "i", "b", "span"],
+        },
       },
-      replace: [' type="text/javascript"']
-    })
+      replace: [' type="text/javascript"'],
+    }),
   ],
 };
