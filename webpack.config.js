6b3c48b6d93bc8ff5629d6ccf401faf7004dc942
LPicker <lianjianjie@foxmail.com>
2020-12-27 01:45:33 +0800

    首页内容布局

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    // publicPath: "/dist/",
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
