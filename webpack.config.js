0463ee8be20b47a1f8ba42523377c50781b6d23e
LPicker <lianjianjie@foxmail.com>
2020-12-29 18:44:03 +0800

    1、支持自动编译并自动刷新浏览器； 2、栏目页面增加分页； 3、首页 banner 增加轮播功能； 4、增加产品详情页面模板；

--- webpack.config.js
+++ webpack.config.js
@@ -10,75 +10,78 @@ const getFileStr = (path) => {
   try {
     str = fs.readFileSync(path, "utf-8");
   } catch (err) {
-    // return "";
   }
   return str;
 };
 
 const pages = fs.readdirSync(path.resolve(__dirname, "src/pages"));
-const htmls = pages.map((page) => {
-  return new htmlWebpackPlugin({
+const loadHtmlPlugns = function (env) {
+  const inject = env !== "production";
+  return pages.map((page) => new htmlWebpackPlugin({
     filename: "pages/" + page,
     template: "src/pages/" + page,
-    inject: false,
+    inject,
     minify: false,
     header: getFileStr("src/layout/header.html"),
     footer: getFileStr("src/layout/footer.html"),
-  });
-});
+  }))
+};
 
-module.exports = {
-  mode: "development",
-  // entry: './src/index.js',
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    // publicPath: "/dist/",
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    watchContentBase: true,
-    compress: true,
-    hot: true,
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: "css-loader"
+module.exports = function (env, argv) {
+  return {
+    // mode: env = "production" ? 'production' : 'development',
+    mode: "development",
+    entry: "./src/index.js",
+    output: {     // webpack-dev-server 不会读取该该属性
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",  // 默认值
     },
-    {
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000
-      }
-    }]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "dist"),
-      dry: false, // 启用删除文件
-    }),
-    new TransferWebpackPlugin(
-      [
-        { from: "src/css", to: "css" },
-        { from: "src/images", to: "images" },
-      ],
-      path.resolve(__dirname, "./")
-    ),
-    ...htmls,
-    new HtmlBeautifyPlugin({
-      config: {
-        html: {
-          end_with_newline: true,
-          indent_size: 2,
-          indent_with_tabs: false,
-          indent_inner_html: true,
-          preserve_newlines: true,
-          unformatted: ["p", "i", "b", "span"],
-        },
+    devServer: {
+      // 没生效
+      contentBase: [path.join(__dirname, "src/layout")],
+      compress: true,
+      hot: true,
+    },
+    module: {
+      rules: [{
+        test: /\.css$/,
+        use: "css-loader"
       },
-      replace: [' type="text/javascript"'],
-    }),
-  ],
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }]
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        root: path.resolve(__dirname, "dist"),
+        dry: false, // 启用删除文件
+      }),
+      new TransferWebpackPlugin(
+        [
+          { from: "src/css", to: "css" },
+          { from: "src/images", to: "images" },
+        ],
+        path.resolve(__dirname, "./")
+      ),
+      ...loadHtmlPlugns(env),
+      new HtmlBeautifyPlugin({
+        config: {
+          html: {
+            end_with_newline: true,
+            indent_size: 2,
+            indent_with_tabs: false,
+            indent_inner_html: true,
+            preserve_newlines: true,
+            unformatted: ["p", "i", "b", "span"],
+          },
+        },
+        replace: ['type="text/javascript"'],
+      }),
+    ],
+  }
 };
