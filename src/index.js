e9b7ed156d150dee8e8e8359ad8f1f0f883352aa
LPicker <lianjianjie@foxmail.com>
2020-12-30 20:04:24 +0800

    产品详情页面

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/layout.css";
 import "./css/index.css";
 import "./css/chanpinku.css";
+import "./css/chanpin.css";
 // import "./layout/header.html";
 // import "./layout/footer.html";
 console.log('hello world')
